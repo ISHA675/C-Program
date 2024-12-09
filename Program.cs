dd49be2b59b29fc2b58130087f3e01fb778d4203
ISHA675 <kcisha675@gmail.com>
2024-12-09 15:28:54 -0600

    All files

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    { 
+        Console.WriteLine("Enter the radius:");
+        var r=Console.ReadLine();
+        var radius=double.Parse(r);
+        double area=calculate_area(radius);
+        Console.WriteLine("The area of circle is:"+area);
+        //Console.ReadKey();
+        //create instance of class
+        //var Circle=new Shape();
+    }
+    static double calculate_area(double r){
+        const double pi=3.14;
+        return (pi*r*r);
+    }
+    
+    
+}
